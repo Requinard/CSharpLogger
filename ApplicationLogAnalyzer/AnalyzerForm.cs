2e39fa764b303add8066cd0f57de126295e03095
Requinard <d.diks@mail.blastedsolutions.com>
2015-03-29 15:23:07 +0200

    Finished background tasks on analyzer

--- ApplicationLogAnalyzer/AnalyzerForm.cs
+++ ApplicationLogAnalyzer/AnalyzerForm.cs
@@ -1,20 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Runtime.Remoting.Messaging;
 using System.Runtime.Serialization;
 using System.Windows.Forms;
 using ApplicationLogger;
 
 namespace ApplicationLogAnalyzer
 {
-    public partial class Form1 : Form
+    public partial class AnalyzerForm : Form
     {
-        public Form1()
+        public AnalyzerForm()
         {
             InitializeComponent();
             Logger.OnNewLogItem += Logger_OnNewLogItem;
+            openLogFileDialog.InitialDirectory = Environment.SpecialFolder.Personal.ToString();
+            dataGridView1.Columns[0].Width = 75;
+            dataGridView1.Columns[1].Width = 125;
+            dataGridView1.Columns[2].Width = dataGridView1.Width - 200;
+
+            dpEnd.Value = System.DateTime.Now;
+
+            dpEnd.ValueChanged += ConstraintControlChanged;
         }
 
         private void Logger_OnNewLogItem(LogItem item)
@@ -62,8 +68,8 @@ namespace ApplicationLogAnalyzer
             dataGridView1.Rows.Clear();
             foreach (LogItem item in log)
             {
-                dataGridView1.Rows.Add(new object[3] { item.Level, item.DateTime, item.Message });
-            } 
+                dataGridView1.Rows.Add(new object[3] {item.Level, item.DateTime, item.Message});
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -80,23 +86,26 @@ namespace ApplicationLogAnalyzer
 
         private void Form1_Load(object sender, EventArgs e)
         {
-           
         }
 
         private void ConstraintControlChanged(object sender, EventArgs e)
         {
-            LogLevel level = (LogLevel)1;
-            if (radioButton2.Checked)
+            var level = (LogLevel) 1;
+            if (rbDebug.Checked)
                 level = LogLevel.Debug;
-            else if (radioButton3.Checked)
+            else if (rbInfo.Checked)
                 level = LogLevel.Info;
-            else if (radioButton4.Checked)
+            else if (rbSuccess.Checked)
                 level = LogLevel.Success;
-            else if (radioButton5.Checked)
+            else if (rbWarning.Checked)
                 level = LogLevel.Warning;
-            else if (radioButton6.Checked)
+            else if (rbError.Checked)
                 level = LogLevel.Error;
-            UpdateColumn(Logger.SortLogItemsByLogLevel(dateTimePicker1.Value, dateTimePicker2.Value, level));
+            UpdateColumn(Logger.SortLogItemsByLogLevel(dpStart.Value, dpEnd.Value, level));
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
         }
     }
 }
--- ApplicationLogAnalyzer/Program.cs
+++ ApplicationLogAnalyzer/Program.cs
@@ -13,7 +13,7 @@ namespace ApplicationLogAnalyzer
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new AnalyzerForm());
         }
     }
 }
